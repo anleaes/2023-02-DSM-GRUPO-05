f3f139d7297544955d93cc4450c36bfb647c80fa
edumengue <mengueeduardo@gmail.com>
2023-10-30 18:55:38 -0300

    socialNetwork

---
+++ Socialnetwork.py
@@ -0,0 +1,7 @@
+class socialnetwork():
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+def showname(self):
+    print("Nome: " + self.name)
