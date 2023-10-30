e3022a4d56de35feed295c1a34de9efd31aefd72
edumengue <mengueeduardo@gmail.com>
2023-10-30 18:42:19 -0300

    Cliente 1

---
+++ Client.py
@@ -0,0 +1,12 @@
+
+class client():
+    def __init__(self, first_name, last_name, address, cell_phone, email, gender):
+        self.first_name = first_name
+        self.last_name = first_name
+        self.address = address
+        self.cell_phone = cell_phone
+        self.email = email
+        self.gender = gender
+
+def showAddress(self):
+    print("Endereço do cliente: " + self.address)
