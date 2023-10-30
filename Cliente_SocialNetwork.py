9b540402127786bffd19d82c086e1c83c9e91b0b
edumengue <mengueeduardo@gmail.com>
2023-10-30 19:00:00 -0300

    cliente_SocialNetwork

---
+++ Cliente_SocialNetwork.py
@@ -0,0 +1,10 @@
+from Client import client
+from Socialnetwork import socialnetwork
+
+class cliente_socialnetwork():
+    def __init__(self, client, socialnetwork):
+        self.client = client
+        self.socialnetwork = socialnetwork
+
+def showClient(self):
+    print("cliente: " + self.cliente)
