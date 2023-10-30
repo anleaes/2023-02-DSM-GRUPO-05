6a30ace6f6407ccb0c43461123065fed36844980
Rippelzin <gabrielrippelima@gmail.com>
2023-10-30 10:19:13 -0300

    metodo show age

--- Product.py
+++ Product.py
@@ -1,4 +1,5 @@
 import Category
+from datetime import datetime
 class Product:
 
     def __init__(self,name,description,date_fabrication, is_active, category):
@@ -9,9 +10,11 @@ class Product:
         self._category = category
 
 
-    def showInfo(self):
-        print('Nome: ' + self._name)
-        print('Descricao: ' + self._description)
-        print('data de fabricacai: ' + self._date_fabrication)
-        print('esta ativado: ' + self._is_active)
-        print('categoria: ' + categoy.showCategory())
+    def showAge(self):
+        if self._is_active:
+            now = datetime.now()
+            age = now - self._date_fabrication
+            age = age.strftime("%Y")
+            print('esse produto esta no mercado a : ' + age  + 'anos')
+        else :
+            print('esse produto esta desativado')
