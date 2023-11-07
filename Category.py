d857fb52491a97393d35949f32b974d6da6cd477
edumeng <12923118156@ulife.com.br>
2023-11-06 21:38:58 -0300

    delete

--- Category.py
+++
@@ -1,9 +0,0 @@
-class Category:
-    def __init__(self, id, name, description):
-        self._id = id
-        self._name = name
-        self._description = description
-    
-    def changeDescription(self, newDescription):
-        self._description = newDescription
-        print('essa e a nova descricao: ' + self._description)
--- Socialnetwork.py
+++
@@ -1,7 +0,0 @@
-class socialnetwork():
-    def __init__(self, name, description):
-        self.name = name
-        self.description = description
-
-def showdescription(self):
-    print("Descrição: " + self.description)
