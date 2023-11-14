785d2f4f23ae2d775b95e1a670966c4d4315ed29
edumeng <12923118156@ulife.com.br>
2023-11-13 21:35:17 -0300

    categories

--- projetoa3/settings.py
+++ projetoa3/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'categories.apps.CategoriesConfig',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
