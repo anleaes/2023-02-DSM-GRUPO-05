ad8a019982f5441264909498982ee328478aef1e
edumengue <mengueeduardo@gmail.com>
2023-11-17 20:16:21 -0300

    clients

--- projetoa3/settings.py
+++ projetoa3/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'clients.apps.ClientsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- projetoa3/urls.py
+++ projetoa3/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('clientes/', include('clients.urls', namespace='clients')),
+    path('clientes_redessociais/', include('clients.urls', namespace='clients_socialnetworks')),
 ]
