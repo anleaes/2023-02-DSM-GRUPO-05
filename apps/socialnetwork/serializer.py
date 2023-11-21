9608210d387f5bcbb314b71117707168ab3fdb7e
edumengue <mengueeduardo@gmail.com>
2023-11-21 12:37:20 -0300

    exclude

--- apps/socialnetwork/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import Socialnetwork
-from rest_framework import serializers
-
-class SocialnetworkSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Socialnetwork
-        fields = '__all__'
