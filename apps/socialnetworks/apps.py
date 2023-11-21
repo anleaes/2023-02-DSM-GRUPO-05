ae2e5232baa9080ee207df58805e5e87cbdb9520
edumengue <mengueeduardo@gmail.com>
2023-11-21 12:40:44 -0300

    exclude

--- apps/socialnetworks/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class SocialnetworkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'socialnetworks'
-    verbose_name = 'Redes Sociais'
