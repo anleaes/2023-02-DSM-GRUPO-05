97466d5629ae69452fe2d48a93e5bb200f26a7cc
edumengue <mengueeduardo@gmail.com>
2023-11-24 16:32:25 -0300

    change

--- apps/avaliacao_fisica/apps.py
+++ apps/avaliacao_fisica/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class AvaliacaoFisicaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'avaliacao_fisica'
-    verbose_name = 'Categorias'
+    verbose_name = 'avaliacao fisica'
