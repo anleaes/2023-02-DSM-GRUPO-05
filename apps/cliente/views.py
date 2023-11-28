942b6cfc896cc5039ef30f794e62eaf6d25e2229
edumengue <mengueeduardo@gmail.com>
2023-11-28 18:51:22 -0300

    cliente views

--- apps/cliente/views.py
+++ apps/cliente/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import cliente, clienteavaliacao, clienteplano
+from rest_framework import viewsets
+from .serializer import clienteSerializer, clienteavaliacaoSerializer, clienteplanoSerializer
 
 # Create your views here.
+
+class clienteViewSet(viewsets.ModelViewSet):
+    queryset = cliente.objects.all()
+    serializer_class = clienteSerializer  
+
+class clienteavaliacaoViewSet(viewsets.ModelViewSet):
+    queryset = clienteavaliacao.objects.all()
+    serializer_class = clienteavaliacaoSerializer
+
+class clienteplanoViewSet(viewsets.ModelViewSet):
+    queryset = clienteplano.objects.all()
+    serializer_class = clienteplanoSerializer
