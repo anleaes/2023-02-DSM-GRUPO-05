aaba29b52a9da9af4509bdf11d5f19ad655529e6
edumengue <mengueeduardo@gmail.com>
2023-11-28 16:30:30 -0300

    views

--- apps/plano/views.py
+++ apps/plano/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import plano
+from rest_framework import viewsets
+from .serializer import planoSerializer
 
 # Create your views here.
+
+class planoViewSet(viewsets.ModelViewSet):
+    queryset = plano.objects.all()
+    serializer_class = planoSerializer 
