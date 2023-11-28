b2bac66bb2faf2270d8c65749b60bd49e8112c8f
edumengue <mengueeduardo@gmail.com>
2023-11-28 17:08:01 -0300

    gfd

--- academia/urls.py
+++ academia/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('plano/', include('plano.urls', namespace='plano')),
+    path('avaliacao/', include('avaliacao.urls', namespace='avaliacao')),
 ]
