ea1b076ae3f0dd354b68bf4e66871657f4fb980b
edumengue <mengueeduardo@gmail.com>
2023-11-28 16:21:44 -0300

    settings_urls

--- apps/avaliacao_fisica/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AvaliacaoFisicaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'avaliacao_fisica'
-    verbose_name = 'avaliacao fisica'
--- apps/avaliacao_fisica/serializer.py
+++
@@ -1,7 +0,0 @@
-from .models import avaliacao_fisica
-from rest_framework import serializers
-
-class avaliacao_fisicaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = avaliacao_fisica
-        fields = '__all__'
