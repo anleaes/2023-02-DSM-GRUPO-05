a82b48cc877ede259b4bafa412517001b0956146
edumengue <mengueeduardo@gmail.com>
2023-11-28 18:47:10 -0300

    cliente serializer

---
+++ apps/cliente/serializer.py
@@ -0,0 +1,18 @@
+from .models import cliente, clienteavaliacao
+from .models import cliente, clienteplano
+from rest_framework import serializers
+
+class clienteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = cliente
+        fields = '__all__'
+        
+class clienteavaliacaoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = clienteavaliacao
+        fields = '__all__'
+
+class clienteplanooSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = clienteplano
+        fields = '__all__'
