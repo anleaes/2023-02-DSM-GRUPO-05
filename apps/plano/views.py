47bd7253103c7bd91a934edbfea000225d5176e4
edumengue <mengueeduardo@gmail.com>
2023-11-28 16:31:22 -0300

    views

--- apps/plano/views.py
+++ apps/plano/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.urls import path, include
+from . import views
+from rest_framework import routers
+
+app_name = 'plano'
+
+router = routers.DefaultRouter()
+router.register('', views.planoViewSet, basename='plano')
+
+urlpatterns = [
+    path('', include(router.urls) )
+]
