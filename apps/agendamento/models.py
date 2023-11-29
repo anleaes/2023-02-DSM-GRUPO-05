9a8a0baf0ef6e267d427c86995d885d55c0d1a32
edumengue <mengueeduardo@gmail.com>
2023-11-29 20:28:33 -0300

    agendamento model

--- apps/agendamento/models.py
+++ apps/agendamento/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from instrutor.models import instrutor
+from cliente.models import cliente
 
 # Create your models here.
+
+class agendamento(models.Model):
+    data = models.CharField('Data', max_length=50)
+    hora = models.CharField('Horário', max_length=100) 
+    local = models.CharField('Endereco', max_length=200)   
+    agendamento_instrutor = models.ManyToManyField(instrutor)
+    agendamento_cliente = models.ManyToManyField(cliente)
+
+    class Meta:
+        verbose_name = 'agendamento'
+        verbose_name_plural = 'agendamentos'
+        ordering =['id']
+
+    def __str__(self):
+        return self.first_name
