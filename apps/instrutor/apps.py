fc3c36dddafb9bf69a1280cd2b0961adfbbd9ed6
edumengue <mengueeduardo@gmail.com>
2023-11-29 19:45:23 -0300

    instrutor_app

---
+++ apps/instrutor/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class InstrutorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'instrutor'
+    verbose_name = 'instrutor'
