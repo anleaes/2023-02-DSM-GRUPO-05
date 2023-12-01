bfee141cfc94e4e0f1b01dd6bf4ce8e73c5b7ca2
comelli19 <leocomellli@hotmail.com>
2023-12-01 19:39:58 -0300

    exercicios app

---
+++ apps/exercicios/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class exerciciosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'exercicios'
+    verbose_name = 'exercicios'
