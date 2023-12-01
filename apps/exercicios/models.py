32d78fda5b15a599e424f700b3aa6dede86b469d
comelli19 <leocomellli@hotmail.com>
2023-12-01 19:42:35 -0300

    models

--- apps/exercicios/models.py
+++ apps/exercicios/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class exercicios(models.Model):
+    name = models.CharField('Nome', max_length=50)
+    description = models.TextField('Descricao', max_length=100)
+    dificuldade = models.CharField('Dificuldade', max_length=50)
+    area_desenvolvimento = models.CharField('Área de desenvolvimento', max_length=50)
+
+
+
+
+    class Meta:
+        verbose_name = 'exercicio'
+        verbose_name_plural = 'exercicios'
+        ordering =['id']
+
+    def __str__(self):
+        return self.name
