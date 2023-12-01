83965aeb19f02dade0769df7e057ba0a187cf3d4
comelli19 <leocomellli@hotmail.com>
2023-12-01 19:46:28 -0300

    views

--- apps/exercicios/views.py
+++ apps/exercicios/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import exercicio
+from rest_framework import viewsets
+from .serializer import exercicioSerializer
 
 # Create your views here.
+
+class exercicioViewSet(viewsets.ModelViewSet):
+    queryset = exercicio.objects.all()
+    serializer_class = exercicioSerializer  
